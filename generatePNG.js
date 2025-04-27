af038c421d54e9e53cf1c8b346acd3c35a5c7650
Taisuke Fukuno <fukuno@jig.jp>
2025-04-27 09:37:36 +0900

    added option for fetchImage

--- generatePNG.js
+++ generatePNG.js
@@ -4,9 +4,10 @@ import { fetchImage, SIZE_PORTRAIT } from "https://code4fukui.github.io/txt2img/
 
 const defpreprompt = "下記のプレゼンテーションの文字起しからポップなグラレコを生成してください";
 
-export const generatePNG = async (txtfn, pngfn, preprompt = defpreprompt) => {
+export const generatePNG = async (txtfn, pngfn, preprompt = defpreprompt, opt = {}) => {
   const txt = await Deno.readTextFile(txtfn);
   const prompt = preprompt + "\n" + txt;
-  const bin = await fetchImage(prompt, { size: SIZE_PORTRAIT });
+  opt.size = opt.size ?? SIZE_PORTRAIT;
+  const bin = await fetchImage(prompt, opt);
   await Deno.writeFile(pngfn, bin);
 };
